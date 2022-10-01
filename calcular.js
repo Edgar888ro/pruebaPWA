7a7ddf38de066685e0ae91beb40b76cf52b23f38
Edgar888ro <edgarro888@gmail.com>
2022-10-01 14:58:49 -0500

    Initial commit

---
+++ calcular.js
@@ -0,0 +1,18 @@
+function Suma(){
+
+    var n1 = Number(document.getElementById("n1").value);
+    var n2 = Number(document.getElementById("n2").value);
+    
+    var T = n1 + n2;
+    
+    document.getElementById("T").value =T;
+}
+function Resta(){
+
+    var n1 = Number(document.getElementById("n1").value);
+    var n2 = Number(document.getElementById("n2").value);
+    
+    var T = n1 - n2;
+    
+    document.getElementById("T").value =T;
+}
